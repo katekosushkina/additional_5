dd0e42d9a76ebbee04e31c822858beaf027ff124
katekosushkina <kate.kosushkina@gmail.com>
2018-09-28 15:25:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let conf =[];
+  let ind = 1;
+  if (str.length%2!=0){
+  return false;
+  }
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+  let arr = bracketsConfig[i];
+  conf[i] = arr.join("");
+  }
+
+while (str.length>0&&ind==1) {
+  ind = 0;
+  for (i = 0; i < conf.length; i++) {
+  let str1 = conf[i];
+  if (str.indexOf(str1)!=-1) {
+  ind = 1;
+  str = str.replace(str1, '');
+  }
+  }
 }
+
+
+  if (str.length==0) {
+  return true;
+  }
+  else {
+  return false;
+  }
+};
